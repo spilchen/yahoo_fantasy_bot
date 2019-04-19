84e1603eecd0f409008630ec0f2136769fa4053c
Matt Spilchen <spilchen@ca.ibm.com>
2019-04-19 08:05:25 -0400

    Add API to build dataset to feed models

--- yahoo_baseball_assistant/hitting.py
+++ yahoo_baseball_assistant/hitting.py
@@ -4,43 +4,71 @@ from pybaseball import batting_stats_range
 import pandas as pd
 
 
-def build_dataset(date_ranges, predict_category, min_PA=None,
-                  generator=batting_stats_range):
-    '''Build the dataset used for hitting predictions
+def build_model_dataset(date_ranges, predict_category, min_PA=None,
+                        generator=batting_stats_range):
+    '''Build the dataset used for building hitting models
 
-    Args:
-      date_ranges (list of ranges)  List of date ranges to use
-        for the dataset.  There must be at least two ranges here.
-        One range that serve as the input data and the second that
-        will serve the predicted value.  If more then two ranges are
-        specified, the first n-1 will be used for the input data and
+    :param date_range: List of date ranges to use for the dataset.  There must
+        be at least two ranges here.  One range that serve as the input data
+        and the second that will serve the predicted value.  If more then two
+        ranges are specified, the first n-1 will be used for the input data and
         the last one will be for the predicted values.
-
-      predict_category (str) The hitting category that we will predict.
+    :type date_range: List of date ranges
+    :param predict_category: The hitting category that we will predict.
         The category name must be one of the columns returned by the
         batting_stats_range() API.
-
-      min_PA (int) If set, we will filter out rows that doesn't exceed
+    :type predict_category: str
+    :param min_PA: If set, we will filter out rows that doesn't exceed
         this minimum plate appearances
-
-      generator (functor) Data generation source.  Use the default, unless
+    :type min_PA: int
+    :param generator: Data generation source.  Use the default, unless
         you are adding dependency injection for testing purposes
-
-    Returns:
-      DataFrame: The dataset of k columns.  The first k-1 columns will be
+    :type generator: function
+    :return: The dataset of k columns.  The first k-1 columns will be
         the input values and the kth column is the predicted value.
+    :rtype: DataFrame
 
-    Examples:
-      df = hitting.build_dataset([['2016-01-01', '2016-12-31'],
-                                  ['2017-01-01', '2017-12-31']],
-                                 'HR')
+    >>> df = hitting.build_model_dataset([['2016-01-01', '2016-12-31'],
+                                         ['2017-01-01', '2017-12-31']],
+                                         'HR')
     '''
     if len(date_ranges) <= 1:
         raise RuntimeError("Must have at least 2 date ranges")
     input_ranges = date_ranges[:-1]
     predict_range = date_ranges[-1]
-    input_data = None
+    input_data = _build_input(input_ranges, generator, min_PA=min_PA)
+    predict_full_data = _transform(
+        generator(predict_range[0], predict_range[1]), min_PA=min_PA)
+    predict_data = pd.DataFrame()
+    predict_data['Name'] = predict_full_data['Name']
+    predict_data[predict_category] = predict_full_data[predict_category]
+    return pd.merge(input_data, predict_data, on='Name')
 
+
+def build_predict_dataset(date_range, generator=batting_stats_range):
+    '''Build the dataset used with a hitting model to predict values.
+
+    :param date_range: A single date range of the baseball data to use
+    :type date_range: List of two dates
+    '''
+    if len(date_range) == 1:
+        raise RuntimeError("Must have at exactly 1 date ranges")
+    return _build_input(date_range, generator)
+
+
+def _build_input(input_ranges, generator, min_PA=None):
+    """Build a data set across multiple date ranges.
+
+    :param input_ranges: Set of 1 or more date ranges.  Each range we will
+       generate stats for and join them into a master data set.
+    :type input_ranges: List of date pairs
+    :param generator: Generator of the data.  Takes as input two date ranges.
+    :type generator: function
+    :param min_PA: If set, we will filter out rows that doesn't exceed
+        this minimum plate appearances
+    :type min_PA: int
+    """
+    input_data = None
     for i, dr in zip(range(len(input_ranges)), input_ranges):
         data = _transform(generator(dr[0], dr[1]), min_PA=min_PA)
         # We're going to join each of the input sets together, so we need
@@ -51,13 +79,7 @@ def build_dataset(date_ranges, predict_category, min_PA=None,
             input_data = data
         else:
             input_data = pd.merge(input_data, data, on='Name')
-
-    predict_full_data = _transform(
-        generator(predict_range[0], predict_range[1]), min_PA=min_PA)
-    predict_data = pd.DataFrame()
-    predict_data['Name'] = predict_full_data['Name']
-    predict_data[predict_category] = predict_full_data[predict_category]
-    return pd.merge(input_data, predict_data, on='Name')
+    return input_data
 
 
 def _transform(df, min_PA=None):
--- yahoo_baseball_assistant/tests/test_hitting.py
+++ yahoo_baseball_assistant/tests/test_hitting.py
@@ -3,48 +3,43 @@
 from yahoo_baseball_assistant import hitting
 import pytest
 import pandas as pd
+import os
 
 
-def test_build_dataset_with_bad_parms():
+def gen_sample_batting_stats(start_date, end_date):
+    """Generate mock data for the batting_stats_range API"""
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    return(pd.read_csv(dir_path + "/sample.batting_stats_range.csv"))
+
+
+def test_build_model_dataset_with_bad_parms():
     with pytest.raises(Exception):
-        hitting.build_dataset(None, 'HR')
+        hitting.build_model_dataset(None, 'HR')
     with pytest.raises(Exception):
-        hitting.build_dataset('2001-01-12', 'HR')
+        hitting.build_model_dataset('2001-01-12', 'HR')
     with pytest.raises(Exception):
-        hitting.build_dataset(['2001-01-12'], 'HR')
+        hitting.build_model_dataset(['2001-01-12'], 'HR')
 
 
 def test_limit_for_small_PA():
-    def generator(start_date, end_date):
-        d = {'Name': {1: 'Jose', 2: 'Ronald', 3: 'Willy', 4: 'Lane'},
-             'Age': {1: 31, 2: 20, 3: 22, 4: 28},
-             '#days': {1: 175, 2: 161, 3: 161, 4: 161},
-             'Lev': {1: 'MLB-AL', 2: 'MLB-NL', 3: 'MLB-AL', 4: 'MLB-NL'},
-             'Tm': {1: 'Chicago', 2: 'Atlanta', 3: 'Tampa Bay', 4: 'Atlanta'},
-             'G': {1: 6, 2: 28, 3: 25, 4: 11},
-             'PA': {1: 27, 2: 127, 3: 98, 4: 8},
-             'AB': {1: 24, 2: 109, 3: 85, 4: 8},
-             'R': {1: 1, 2: 20, 3: 13, 4: 5},
-             'H': {1: 3, 2: 33, 3: 29, 4: 2},
-             '2B': {1: 0, 2: 5, 3: 2, 4: 1},
-             '3B': {1: 0, 2: 3, 3: 0, 4: 0},
-             'HR': {1: 0, 2: 4, 3: 2, 4: 1},
-             'RBI': {1: 0, 2: 16, 3: 11, 4: 2},
-             'BB': {1: 1, 2: 16, 3: 13, 4: 0},
-             'IBB': {1: 0, 2: 2, 3: 2, 4: 0},
-             'SO': {1: 7, 2: 31, 3: 26, 4: 4},
-             'HBP': {1: 2, 2: 1, 3: 0, 4: 0},
-             'SH': {1: 0, 2: 0, 3: 0, 4: 0},
-             'SF': {1: 0, 2: 1, 3: 0, 4: 0},
-             'GDP': {1: 0, 2: 1, 3: 3, 4: 0},
-             'SB': {1: 0, 2: 5, 3: 0, 4: 1},
-             'CS': {1: 0, 2: 1, 3: 2, 4: 0},
-             'BA': {1: 0.125, 2: 0.303, 3: 0.341, 4: 0.25},
-             'OBP': {1: 0.222, 2: 0.394, 3: 0.429, 4: 0.25},
-             'SLG': {1: 0.125, 2: 0.514, 3: 0.435, 4: 0.75},
-             'OPS': {1: 0.347, 2: 0.907, 3: 0.864, 4: 1.0}}
-        return pd.DataFrame(data=d)
-    ds = hitting.build_dataset(['2018-09-01', '2018-09-10'], 'HR',
-                               generator=generator,
-                               min_PA=100)
-    assert(len(ds) == 1)
+    dt = ['2018-09-01', '2018-09-10']
+    ds_full = hitting.build_model_dataset(dt, 'HR',
+                                          generator=gen_sample_batting_stats)
+    ds_filt = hitting.build_model_dataset(dt, 'HR',
+                                          generator=gen_sample_batting_stats,
+                                          min_PA=120)
+    assert(len(ds_filt) < len(ds_full))
+
+
+def test_build_predict_dataset_with_parms():
+    with pytest.raises(Exception):
+        hitting.build_predict_dataset(None, generator=gen_sample_batting_stats)
+    with pytest.raises(Exception):
+        hitting.build_predict_dataset('2001-01-12',
+                                      generator=gen_sample_batting_stats)
+
+
+def test_build_predict_dataset():
+    dt = ['2018-09-01', '2018-09-10']
+    df = hitting.build_predict_dataset(dt, generator=gen_sample_batting_stats)
+    assert(len(df) > 0)
