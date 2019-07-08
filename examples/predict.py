953cfdcc7f8f3e19877e648a720dd56d984cb0cc
Matt Spilchen <spilchen@ca.ibm.com>
2019-07-07 20:38:51 -0400

    Predict hitting stats against all teams

--- examples/predict.py
+++ examples/predict.py
@@ -3,16 +3,57 @@
 """Predict the stats for the players currently on your roster
 
 Usage:
-  predict.py <json>
+  predict.py [-o <team> | -a] <json>
 
-  <json>  The name of the JSON that has bearer token.  This can be generated
-          from init_oauth_env.py.
+  <json>     The name of the JSON that has bearer token.  This can be generated
+             from init_oauth_env.py.
+
+Options to pick the team.  If any of these are ommitted, then we'll only
+predict against the team you are playing next week:
+  -a              Predict against all teams.
+  -o <opp_team>   Opponent team to predict against.
 """
 from docopt import docopt
 from yahoo_oauth import OAuth2
 from yahoo_fantasy_api import league, game, team
 from yahoo_baseball_assistant import hitting
 import logging
+
+
+def print_team(team_name, df, my_sum):
+    print("")
+    print("Team Name: {}".format(team_name))
+    columns = ['name', 'team', 'WK_G', 'G', 'AB', 'R', '1B', '2B', '3B',
+               'HR', 'RBI', 'BB', 'SO', 'SB', 'AVG', 'OBP']
+    cformat = "{:20} {:5} {:5} {:5} {:5} {:5} {:5} {:5} {:5} {:5} {:5} " \
+        "{:5} {:5} {:5} {:5} {:5}"
+    print(cformat.format(*columns))
+    for plyr in df.iterrows():
+        print(cformat.format(*[plyr[1][x] for x in columns]))
+    print("")
+    print("Stat prediction for week")
+    print(my_sum)
+
+
+def get_opp_teams(args, lg, my_tm):
+    teams = []
+    if args['-a']:
+        for tm in lg.teams():
+            if tm['team_key'] == my_tm.team_key:
+                continue
+            teams.append(tm)
+    elif args['-o']:
+        for tm in lg.teams():
+            if tm['name'] == args['-o']:
+                teams.append(tm)
+                break
+    else:
+        key = my_tm.matchup(lg.current_week() + 1)
+        for tm in lg.teams():
+            if tm['team_key'] == key:
+                teams.append(tm)
+                break
+    return teams
 
 
 if __name__ == '__main__':
@@ -23,17 +64,20 @@ if __name__ == '__main__':
     gm = game.Game(sc, 'mlb')
     league_id = gm.league_ids(year=2019)
     lg = league.League(sc, league_id[0])
-    cur_wk = lg.current_week()
-    if cur_wk >= lg.end_week():
-        raise RuntimeError("Season over no more weeks to predict")
     team_key = lg.team_key()
     my_tm = team.Team(sc, team_key)
-    bldr = hitting.Builder(my_tm, cur_wk + 1)
+    bldr = hitting.Builder(lg, my_tm)
     df = bldr.roster_predict()
-    columns = ['name', 'G', 'AB', 'R', '1B', '2B', '3B', 'HR', 'RBI', 'BB',
-               'SO', 'SB', 'AVG', 'OBP']
-    cformat = "{:20} {:5} {:5} {:5} {:5} {:5} {:5} {:5} {:5} {:5} " \
-        "{:5} {:5} {:5} {:5}"
-    print(cformat.format(*columns))
-    for plyr in df.iterrows():
-        print(cformat.format(*[plyr[1][x] for x in columns]))
+    my_sum = bldr.sum_prediction(df)
+    print_team("Lumber Kings", df, my_sum)
+
+    # Compare against a bunch of teams
+    teams = get_opp_teams(args, lg, my_tm)
+    for tm in teams:
+        opp_tm = team.Team(sc, tm['team_key'])
+        bldr = hitting.Builder(lg, opp_tm)
+        df = bldr.roster_predict()
+        opp_sum = bldr.sum_prediction(df)
+        print_team(tm['name'], df, opp_sum)
+        (w, l) = bldr.score(my_sum, opp_sum)
+        print("Prediction result: {} - {}".format(w, l))
--- yahoo_baseball_assistant/hitting.py
+++ yahoo_baseball_assistant/hitting.py
@@ -1,6 +1,6 @@
 #!/bin/python
 
-from baseball_scraper import fangraphs
+from baseball_scraper import fangraphs, baseball_reference
 from baseball_id import Lookup
 import pandas as pd
 
@@ -16,14 +16,18 @@ class Builder:
     :param week: Week number to build the predictions for
     :type week: int
     """
-    def __init__(self, team, week):
+    def __init__(self, lg, team):
         self.id_lookup = Lookup
         self.fg = fangraphs.Scraper()
         self.team = team
-        self.week = week
+        self.week = lg.current_week() + 1
+        if self.week >= lg.end_week():
+            raise RuntimeError("Season over no more weeks to predict")
+        (self.start_date, self.end_date) = lg.week_date_range(self.week)
+        self.mlb_team = {}
         self.roster = None
         if team is not None:
-            self.roster = team.roster(week)
+            self.roster = team.roster(self.week)
 
     def set_id_lookup(self, lk):
         self.id_lookup = lk
@@ -42,27 +46,117 @@ class Builder:
         """
         lk = self._find_roster()
         df = pd.DataFrame()
-        for fg_id, name in zip(lk['fg_id'], lk['yahoo_name']):
+        for fg_id, name, team in zip(lk['fg_id'], lk['yahoo_name'],
+                                     lk['mlb_team']):
+            # TODO: use lahman database instead of this mapping.  The
+            # abbreviation from baseball_id is different then at
+            # baseball_reference.
+            if team == 'WSH':
+                team = 'WSN'
+            if team == 'CWS':
+                team = 'CHW'
             self.fg.set_player_id(fg_id)
             scrape_series = self.fg.scrape(instance='Steamer (R)').iloc(0)[0]
-            meta_series = pd.Series(data=[fg_id, name],
-                                    index=['fg_id', 'name'])
+            wk_games = self._num_games_for_team(team)
+            meta_series = pd.Series(data=[fg_id, name, team, wk_games],
+                                    index=['fg_id', 'name', 'team',
+                                           'WK_G'])
             combined_series = scrape_series.append(meta_series)
             df = df.append(combined_series, ignore_index=True)
         return df
 
+    def sum_prediction(self, df):
+        """Summarize the predictions into stat categories
+
+        :param df: Roster predictions to summarize
+        :type df: DataFrame
+        :return: Summarized predictions
+        :rtype: Series
+        """
+        res = pd.Series()
+        for stat in ['R', 'HR', 'RBI', 'SB', 'AB', 'H', 'BB']:
+            val = 0
+            for plyr in df.iterrows():
+                if plyr[1]['G'] > 0:
+                    val += plyr[1][stat] / plyr[1]['G'] * plyr[1]['WK_G']
+            res = res.append(pd.Series(data=[val], index=[stat]))
+
+        # Handle ratio stats
+        if res['AB'] > 0:
+            avg = res['H'] / res['AB']
+        else:
+            avg = 0
+        if res['AB'] + res['BB'] > 0:
+            oba = (res['H'] + res['BB']) / (res['AB'] + res['BB'])
+        else:
+            oba = 0
+        res = res.append(pd.Series(data=[avg, oba], index=['AVG', 'OBP']))
+
+        # Delete the temporary values used to calculate the ratio stats
+        res = res.drop(index=['AB', 'H', 'BB'])
+
+        return res
+
+    def score(self, left, right):
+        """Determine how many points comparing two summarized stats together
+
+        :param left: Summarized stats to compare
+        :type left: Series
+        :param right: Summarized stats to compare
+        :type right: Series
+        :return: Number of wins and number of losses.
+        :rtype: Tuple of two ints
+        """
+        (win, loss) = (0, 0)
+        for l, r in zip(left, right):
+            if l > r:
+                win += 1
+            elif r > l:
+                loss += 1
+        return (win, loss)
+
     def _find_roster(self):
-        yahoo_ids = [x['player_id'] for x in self.roster if
-                     (x['position_type'] == 'B' and
-                      x['selected_position'] != 'BN')]
-        lk = self.id_lookup.from_yahoo_ids(yahoo_ids)
-        # Do a lookup of names.  This lookup will find any player that doesn't
-        # have a yahoo_id in the lookup cache.  This is done for new rookies
-        # that are on the roster.
-        names = [x['name'] for x in self.roster if
-                 (x['position_type'] == 'B' and
-                  x['selected_position'] != 'BN')]
-        # Union the two lookup to have a complete picture of the roster
-        lk_miss = self.id_lookup.from_names(names, filter_missing='yahoo_id')
-        lk = lk.merge(lk_miss, how='outer')
+        lk = None
+        for plyr in self.roster:
+            if plyr['position_type'] != 'B' or \
+                    plyr['selected_position'] in ['BN', 'DL']:
+                continue
+
+            one_lk = self.id_lookup.from_yahoo_ids([plyr['player_id']])
+            # Do a lookup of names if the ID lookup didn't work.  We do two of
+            # them.  The first one is to filter on any name that has a missing
+            # yahoo_id.  This is better then just a plain name lookup because
+            # it has a better chance of being unique.  A missing ID typically
+            # happens for rookies.
+            if len(one_lk.index) == 0:
+                one_lk = self.id_lookup.from_names(
+                    [plyr['name']], filter_missing='yahoo_id')
+                # Failback to a pure-name lookup.  There have been instances
+                # with hitter/pitchers where they have two IDs: one for
+                # pitchers and one for hitters.  The id_lookup only keeps
+                # track of one of those IDs.  We will strip off the '(Batter)'
+                # from their name.
+                if len(one_lk.index) == 0:
+                    paren = plyr['name'].find('(')
+                    if paren > 0:
+                        name = plyr['name'][0:paren-1].strip()
+                    else:
+                        name = plyr['name']
+                    one_lk = self.id_lookup.from_names([name])
+
+            if len(one_lk.index) == 0:
+                raise ValueError("Was not able to lookup player: {}".format(
+                    plyr))
+
+            if lk is None:
+                lk = one_lk
+            else:
+                lk = lk.merge(one_lk, how='outer')
         return lk
+
+    def _num_games_for_team(self, abrev):
+        if abrev not in self.mlb_team:
+            self.mlb_team[abrev] = baseball_reference.TeamScraper(abrev)
+            self.mlb_team[abrev].set_date_range(self.start_date, self.end_date)
+        df = self.mlb_team[abrev].scrape()
+        return len(df.index)
