36338f01e8e632c56241dee68a20189bbe1e9a38
Matt Spilchen <spilchen@gmail.com>
2019-12-23 12:26:47 -0500

    Add full lineup optimization as option

--- yahoo_fantasy_bot/automation.py
+++ yahoo_fantasy_bot/automation.py
@@ -1,6 +1,5 @@
 #!/bin/python
 
-import random
 from yahoo_fantasy_bot import bot
 
 
@@ -9,11 +8,14 @@ class Driver(object):
     Driver to do automated actions with the bot.
 
     :param cfg: ConfigParser read in
+    :param dry_run: True if no writes to the Yahoo APIs
+    :param full: True if we are to optimize using the free agents.  False means
+        we just optimize for our bench.
     """
-    def __init__(self, cfg, dry_run, full_prob):
+    def __init__(self, cfg, dry_run, full):
         self.bot = bot.ManagerBot(cfg)
         self.dry_run = dry_run
-        self.full_prob = full_prob
+        self.full = full
 
     def run(self):
         self.bot.sync_lineup()
@@ -22,7 +24,7 @@ class Driver(object):
         print("Adjusting lineup for player status")
         self.bot.pick_injury_reserve()
         self.bot.move_non_available_players()
-        if self._do_full_lineup_overhaul():
+        if self.full:
             print("Optimizing full lineup using available free agents")
             self.bot.optimize_lineup_from_free_agents()
             self.bot.pick_bench()
@@ -37,6 +39,3 @@ class Driver(object):
         self.bot.print_roster()
         print("Computing roster moves to apply")
         self.bot.apply_roster_moves(dry_run=self.dry_run)
-
-    def _do_full_lineup_overhaul(self):
-        return random.randint(0, 100) <= int(self.full_prob)
--- ybot.py
+++ ybot.py
@@ -3,7 +3,7 @@
 """A bot that acts as a manager for Yahoo! fantasy team
 
 Usage:
-  ybot.py [-id] [--full=<pct>] <cfg_file>
+  ybot.py [-idf] <cfg_file>
 
   <cfg_file>  The name of the configuration file.  See sample_config.ini for
               the format.
@@ -12,13 +12,11 @@ Options:
   -i, --interactive   Run the program in interactive mode.
   -d, --dry-run       Does a dry run of the roster change.  No roster change
                       will actually occur.
-  --full=<pct>        Does a full analysis of the entire lineup meaning all
+  -f, --full          Does a full analysis of the entire lineup meaning all
                       players will be evaluated in an optimized lineup.  This
                       can potentially add a lot of churn to your lineup.  The
                       alternative is to just consider optimizing the open slots
-                      in the lineup and the bench.  The value for this option
-                      is a probability that a full optimization will occur.
-                      The value ranges from 0-100.    [Default: 10]
+                      in the lineup and the bench.
 
 """
 from docopt import docopt
